980370ef48ab4c8e64aa7e01a984daca3be81b34
d-arde <dardeleanu00@gmail.com>
2022-09-24 16:36:36 +0100

    removed uneccesary libraries

--- mytutor-lesson-bot.py
+++ mytutor-lesson-bot.py
@@ -1,12 +1,10 @@
 import requests
 import urllib.request
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import pandas as pd
-import json
 import os
 import time
 #check imports
