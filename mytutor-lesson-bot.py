6d4f29d741e820d560b14a5feac89f8d0096abde
d-arde <dardeleanu00@gmail.com>
2022-09-24 16:26:13 +0100

    removed for loop as was not necessary. added ids of buttons and what not - progress

--- mytutor-lesson-bot.py
+++ mytutor-lesson-bot.py
@@ -21,28 +21,24 @@ chrome_prefs["profile.default_content_settings"] = {"images": 2}
 chrome_prefs["profile.managed_default_content_settings"] = {"images": 2}
 
 
-driver = webdriver.Chrome(executable_path=r'C:\\vscode\\webdriver\\chromedriver') 
+driver = webdriver.Chrome(executable_path='/Users/dani/vscode/chromedriver') 
 #download webdriver - change path accordingly
 
 #recurring slots
-for i in range(10):#10 is temporary solution
-    driver.get(base_url) #open mytutor
-    lesson_button = driver.find_element(By.LINK_TEXT, 'View') #get button element of lesson
-    lesson_button.click() #press accept button
 
-    for i in range(10):
-        r = driver.find_element(By.LINK_TEXT, 'Accept') #finds button that says accept
-        r.click() #clicks button/element
+driver.get(base_url) #open mytutor
+
+time.sleep(1)
+
+lesson_button = driver.find_element(By.XPATH, '//*[@id="recurringslotstable:0:view"]') #get button element of lesson
+lesson_button.click() #press accept button
+
+time.sleep(0.2)
+
+r = driver.find_element(By.XPATH, '//*[@id="recurringslotdetailsform:acceptRecurringSlot"]/span') #finds button that says accept
+r.click() #clicks button/element
 
 
-# time.sleep(5)
-
-
-# driver.find_element(By.LINK_TEXT,'Store details').click()
-
-# time.sleep(10)
-
-# c = driver.find_element(By.XPATH,'//*[@id="schema-location"]/script')
 
 
 
